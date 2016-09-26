6c4aa926dcd51012a2e6fd8a1809236c94392a09
Iman Tabrizian <tabrizian@outlook.com>
2016-09-26 15:08:01 +0000

    Adds hello world in the khar library

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 int main(int argc, char **argv) {
     serial::Serial my_serial("/dev/ttyUSB0", 9600, serial::Timeout::simpleTimeout(1000));
 
-    size_t bytes_wrote = my_serial.write("Hello");
+    uint8_t dataArray[] = {0x01, 0x01, 0x30, 0x00, 0x00, 0x01, 0xf2, 0xca};
+    size_t bytes_wrote = my_serial.write(dataArray, 8);
     cout << bytes_wrote << ", Bytes read: ";
+    my_serial.read(1);
 }
