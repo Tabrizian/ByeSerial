21bc8ff18b878343127c1d191015ef412a89dc0b
Iman Tabrizian <tabrizian@outlook.com>
2016-09-26 14:52:36 +0000

    Basic serial handling added

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
+#include <iostream>
+
 #include "serial/serial.h"
 
-int main() {
-    serial::Serial serial("/dev/ttyUSB0");
+using namespace std;
+
+int main(int argc, char **argv) {
+    serial::Serial my_serial("/dev/ttyUSB0", 9600, serial::Timeout::simpleTimeout(1000));
+
+    size_t bytes_wrote = my_serial.write("Hello");
+    cout << bytes_wrote << ", Bytes read: ";
 }
