7216f81cf0918b6d8edd93626cd176a9a3cd5da6
Iman Tabrizian <Tabrizian@outlook.com>
2016-09-27 07:04:54 +0330

    Some minor improvements

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
+#include <cstring>
+#include <cstdlib>
 
 #include "serial/serial.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
-    serial::Serial my_serial("/dev/ttyUSB0", 9600, serial::Timeout::simpleTimeout(1000));
+    serial::Serial serial("/dev/ttyUSB0", 9600,
+            serial::Timeout::simpleTimeout(1000));
 
-    uint8_t dataArray[] = {0x01, 0x01, 0x30, 0x00, 0x00, 0x01, 0xf2, 0xca};
-    size_t bytes_wrote = my_serial.write(dataArray, 8);
-    cout << bytes_wrote << ", Bytes read: ";
-    my_serial.read(1);
+    if(argc == 1) {
+        serial.write(argv[0]);
+    } else if (argc == 2) {
+        if(strcmp(argv[0], "-r") == 0) {
+            string read = serial.read(atoi(argv[1]));
+            cout << read;
+        }
+    }
 }
