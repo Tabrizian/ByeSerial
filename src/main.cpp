7738a517b40ebefa83e701d2385bf4e97c6c27bb
Iman Tabrizian <tabrizian@outlook.com>
2016-09-28 11:53:50 +0000

    Always return 0 :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,48 @@
 #include <iostream>
 #include <cstring>
+#include <string>
+#include <sstream>
+#include <iomanip>
 #include <cstdlib>
+#include <algorithm>
 
 #include "serial/serial.h"
 
 using namespace std;
 
-uint8_t* hexToInt(char *arr, int len, uint8_t* result){
+void hexToInt(char *arr, int len, uint8_t* result){
     int halfOfLen = len / 2;
     for(int i = 0; i < halfOfLen; i++) {
         char token[2];
         sprintf(token, "%.*s", 2, arr);
         int number = (int) strtol(token, NULL, 16);
         result[i] = number;
-        cout << "i is " << i << " : " << number << endl;
         arr = arr + 2;
+    }
+}
+
+void intToHex(string bytesArray) {
+    int len = bytesArray.length();
+    for(int i = 0; i < len; i++) {
+        printf("%02x\n", (int) bytesArray[i]);
     }
 }
 
 int main(int argc, char **argv) {
     serial::Serial serial("/dev/ttyUSB0", 9600,
-            serial::Timeout::simpleTimeout(1000));
+            serial::Timeout::simpleTimeout(100));
 
-    if(argc == 2) {
+    if(argc == 3) {
         int sizeOfData = strlen(argv[1]);
         uint8_t result[sizeOfData / 2];
         hexToInt(argv[1], sizeOfData, result);
-        for(int i = 0; i < sizeOfData / 2; i++) {
-            cout << result[i] << " data .. " <<endl;
-        }
         serial.write(result, sizeOfData / 2);
         serial.flush();
-    } else if (argc == 3) {
-        if(strcmp(argv[0], "-r") == 0) {
-            string read = serial.read(atoi(argv[1]));
-            cout << read;
-        }
+        string toRead;
+        int dataRead = serial.read(toRead, atoi(argv[2]));
+        intToHex(toRead);
     }
     serial.close();
+
+    return 0;
 }
