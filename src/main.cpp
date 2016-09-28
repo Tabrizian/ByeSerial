2ca362bdc6b04daffec9fb423084609ff2eaee4f
Iman Tabrizian <tabrizian@outlook.com>
2016-09-28 07:33:22 +0000

    Writing the binary string done

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,14 @@
 
 using namespace std;
 
-int* hexToInt(char *arr, int len){
+uint8_t* hexToInt(char *arr, int len){
     int halfOfLen = len / 2;
+    uint8_t res[halfOfLen];
     for(int i = 0; i < halfOfLen; i++) {
         char token[2];
         sprintf(token, "%.*s", 2, arr);
         int number = (int) strtol(token, NULL, 16);
+        res[i] = number;
         cout << "i is " << i << " : " << number << endl;
         arr = arr + 2;
     }
@@ -23,10 +25,9 @@ int main(int argc, char **argv) {
 
     if(argc == 2) {
         int sizeOfData = strlen(argv[1]);
-        hexToInt(argv[1], sizeOfData);
-        //uint8_t arr[sizeOfData];
-        //serial.write(arr, sizeOfData);
-        //serial.flush();
+        uint8_t* readyToWrite = hexToInt(argv[1], sizeOfData);
+        serial.write(readyToWrite, sizeOfData / 2);
+        serial.flush();
     } else if (argc == 3) {
         if(strcmp(argv[0], "-r") == 0) {
             string read = serial.read(atoi(argv[1]));
