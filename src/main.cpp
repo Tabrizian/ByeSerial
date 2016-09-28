051a894dd9eda5a317a45fc3322e2c00492f7983
Iman Tabrizian <tabrizian@outlook.com>
2016-09-28 07:45:24 +0000

    Reading data complete!

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,13 @@
 
 using namespace std;
 
-uint8_t* hexToInt(char *arr, int len){
+uint8_t* hexToInt(char *arr, int len, uint8_t* result){
     int halfOfLen = len / 2;
-    uint8_t res[halfOfLen];
     for(int i = 0; i < halfOfLen; i++) {
         char token[2];
         sprintf(token, "%.*s", 2, arr);
         int number = (int) strtol(token, NULL, 16);
-        res[i] = number;
+        result[i] = number;
         cout << "i is " << i << " : " << number << endl;
         arr = arr + 2;
     }
@@ -25,8 +24,12 @@ int main(int argc, char **argv) {
 
     if(argc == 2) {
         int sizeOfData = strlen(argv[1]);
-        uint8_t* readyToWrite = hexToInt(argv[1], sizeOfData);
-        serial.write(readyToWrite, sizeOfData / 2);
+        uint8_t result[sizeOfData / 2];
+        hexToInt(argv[1], sizeOfData, result);
+        for(int i = 0; i < sizeOfData / 2; i++) {
+            cout << result[i] << " data .. " <<endl;
+        }
+        serial.write(result, sizeOfData / 2);
         serial.flush();
     } else if (argc == 3) {
         if(strcmp(argv[0], "-r") == 0) {
