304e7939501c324bad19a4e7ef0f56db8ba62bf9
Iman Tabrizian <tabrizian@outlook.com>
2016-09-29 08:43:15 +0000

    Now code works successfuly with input and output

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "serial/serial.h"
 
 using namespace std;
+serial::Serial serial_instance;
 
 void hexToInt(char *arr, int len, uint8_t* result){
     int halfOfLen = len / 2;
@@ -24,25 +25,72 @@ void hexToInt(char *arr, int len, uint8_t* result){
 void intToHex(string bytesArray) {
     int len = bytesArray.length();
     for(int i = 0; i < len; i++) {
-        printf("%02x\n", (int) bytesArray[i]);
+        printf("%02x", (int) bytesArray[i]);
+    }
+    printf("\n");
+}
+
+void boot() {
+    serial_instance.setPort("/dev/ttyUSB0");
+    serial::Timeout timeout = serial::Timeout::simpleTimeout(100);
+    serial_instance.setTimeout(timeout);
+    serial_instance.open();
+    if(serial_instance.isOpen()) {
+        cout << "It is open" << endl;
     }
 }
 
 int main(int argc, char **argv) {
-    serial::Serial serial("/dev/ttyUSB0", 9600,
-            serial::Timeout::simpleTimeout(100));
 
-    if(argc == 3) {
-        int sizeOfData = strlen(argv[1]);
-        uint8_t result[sizeOfData / 2];
-        hexToInt(argv[1], sizeOfData, result);
-        serial.write(result, sizeOfData / 2);
-        serial.flush();
-        string toRead;
-        int dataRead = serial.read(toRead, atoi(argv[2]));
-        intToHex(toRead);
+    boot();
+    int read;
+    int sizeOfData;
+    cin >> read;
+    cin >> sizeOfData;
+    while(true) {
+        if(read == 0) {
+            char data[sizeOfData];
+            cin >> data;
+            uint8_t result[sizeOfData / 2];
+            hexToInt(data, sizeOfData, result);
+            serial_instance.write(result, sizeOfData / 2);
+            serial_instance.flush();
+        } else if (read == 1) {
+            string toRead;
+            int dataRead = serial_instance.read(toRead, sizeOfData);
+            intToHex(toRead);
+        } else {
+            return 0;
+        }
+        cin >> read;
+        cin >> sizeOfData;
     }
-    serial.close();
+    //if(argc == 4) {
+    //    if(strcmp(argv[2], "-r") == 0) {
+    //        int sizeOfData = strlen(argv[1]);
+    //        uint8_t result[sizeOfData / 2];
+    //        hexToInt(argv[1], sizeOfData, result);
+    //        serial->write(result, sizeOfData / 2);
+    //        serial.flush();
+    //        string toRead;
+    //        int dataRead = serial.read(toRead, atoi(argv[2]));
+    //        intToHex(toRead);
+    //    }
+    //} else if (argc == 3) {
+    //    if(strcmp(argv[1], "-r") == 0) {
+    //        string toRead;
+    //        int dataRead = serial.read(toRead, atoi(argv[2]));
+    //        intToHex(toRead);
+    //    }
+    //} else if(argc == 2) {
+    //    int sizeOfData = strlen(argv[1]);
+    //    uint8_t result[sizeOfData / 2];
+    //    hexToInt(argv[1], sizeOfData, result);
+    //    serial.write(result, sizeOfData / 2);
+    //    serial.flush();
+    //}
 
+
+    //serial.close();
     return 0;
 }
